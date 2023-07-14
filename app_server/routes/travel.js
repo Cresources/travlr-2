e057f849bb36cfd02872516acbe39b4139aad002
Cresources <Cresources181@gmail.com>
2023-07-14 16:55:13 +0000

    setup project using express generator

--- app_server/routes/travel.js
+++ app_server/routes/travel.js
@@ -1,32 +1,9 @@
-// const express = require('express'); 
-// const router = express.Router(); 
-// const controller= require('../controllers/travel'); 
- 
-// /* GET home page. */ 
-// router.get('/', controller.travel); 
- 
-// module.exports = router; 
-
-
-// const express = require('express');
-// const router = express.Router();
-
-// // Import the travel controller
-// const travelController = require('../controllers/travel');
-
-// // Define the GET route with the appropriate callback function
-// router.get('/', travelController.travel);
-
-// module.exports = router;
-
 const express = require('express');
 const router = express.Router();
+const controller = require('../controllers/travel');
 
 // GET travel view
-router.get('/', (req, res) => {
-  res.render('travel', { title: 'Travlr Getaways' });
-});
-
+router.get('/', controller.travel);
 module.exports = router;
 
 
