d6f6b486424f3790eca458ba90508c72b44934f6
gagan <Cresources181@gmail.com>
2023-07-30 11:50:27 +0000

    complete module 6

--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -1,6 +1,59 @@
 const mongoose = require('mongoose');
 const Model = mongoose.model('trips');
 
+
+const tripsUpdateTrip = async (req, res) => {
+    console.log(req.body);
+    Model
+    .findOneAndUpdate({ 'code': req.params.tripCode }, {
+        code: req.body.code,
+        name: req.body.name,
+        length: req.body.length,
+        start: req.body.start,
+        resort: req.body.resort,
+        perPerson: req.body.perPerson,
+        image: req.body.image,
+        description: req.body.description
+    }, { new: true })
+    .then(trip => {
+        if (!trip) {
+            return res
+            .status(404)
+            .send({
+                message: "Trip not found with code " + req.params.tripCode
+            });
+        }
+        res.send(trip);
+    }).catch(err => {
+        if (err.kind === 'ObjectId') {
+            return res.status(404).send({
+                message: "Trip not found with code " + req.params.tripCode
+            });
+        }
+        return res.status(500).json(err);
+    });
+}
+
+const tripsAddTrip = async (req, res) => {
+    Model
+    .create({
+        code: req.body.code,
+        name: req.body.name,
+        length: req.body.length,
+        start: req.body.start,
+        resort: req.body.resort,
+        perPerson: req.body.perPerson,
+        image: req.body.image,
+        description: req.body.description
+    }, (err, trip) => {
+        if (err) {
+            console.log(err);
+            return res.status(400).json(err);
+        }
+        else res.status(201).json(trip);
+    })
+}
+
 // GET /trips - list all the trips
 const tripsList = async (req, res) => {
     Model.find({})
@@ -24,5 +77,7 @@ const tripsFindByCode = async (req, res) => {
 
 module.exports = {
     tripsList,
-    tripsFindByCode
+    tripsFindByCode,
+    tripsAddTrip,
+    tripsUpdateTrip
 }
--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const ctrlTrips= require('../controllers/trips');
+const tripsController = require('../controllers/trips')
 
 /* GET trips page. */
-router.get('/trips', ctrlTrips.tripsList);
+router.route('/trips')
+    .get(ctrlTrips.tripsList)
+    .post(tripsController.tripsAddTrip)
+    
 
-router.get('/trips/:tripCode', ctrlTrips.tripsFindByCode);
+router.route('/trips/:tripCode')
+    .get(ctrlTrips.tripsFindByCode)
+    .put(tripsController.tripsUpdateTrip);
 
 module.exports = router;
